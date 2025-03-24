26a959469d3e5a4380cd91c7c8b455da32b6fc26
Jepy <jmoslander15@gmail.com>
2025-03-24 18:29:02 -0500

    first commit. problem one done

--- common-string-methods.js
+++ common-string-methods.js
@@ -6,18 +6,42 @@ Activity 1: Searching Strings
 Write a program to:
 Check if the text "JavaScript" is in the string "Learning JavaScript is fun!" using includes.
 Find the position of the word "fun" in the string.
+*/
+let activityOne = "Learning JavaScript is fun!";
+console.log(activityOne.includes("JavaScript"));
 
+console.log(activityOne.indexOf("fun"));
+
+/*
 Activity 2: Transforming Strings
 Convert the string " CODE BOOTCAMP " to lowercase and remove all extra whitespace.
 Replace "BOOTCAMP" with "JavaScript" in the transformed string.
+*/
+let activityTwo = " CODE BOOTCAMP ";
+let activityTwoLower = activityTwo.toLowerCase();
+let activityTwoTransformed = activityTwoLower.trim();
+console.log(activityTwoTransformed)
 
+let activityTwoReplaced = activityTwoTransformed.replace("bootcamp", "JavaScript");
+console.log(activityTwoReplaced);
+
+/*
 Activity 3: Breaking Apart a Sentence
 Split the sentence "Coding is fun and educational" into an array of words.
+*/
+let activityThree = "Coding is fun and educational";
+let activityThreeArray = activityThree.split(" ");
+console.log(activityThreeArray)
 
+/*
 Activity 4: Retrieving Substrings
 Retrieve the first character of "Bootcamp" using charAt.
 Extract the word "camp" from "Bootcamp" using slice.
+*/
+let activityFour = "Bootcamp"
+let activityFourRetrieve = activityFour.charAt(0)
 
+/*
 Advanced Challenge
 Write a program to process the following string:
 Customer: John Doe
@@ -26,7 +50,20 @@ Total: $20.50
 Extract the customer name.
 Split the order into an array of items.
 Convert the total price to uppercase (e.g., "TOTAL: $20.50").
+*/
+let advancedChallenge = `Customer: John Doe
+Order: Apple, Banana, Grape
+Total: $20.50`;
 
+let advancedChallengeName = advancedChallenge.slice(10,18);
+let advancedChallengeArrayChunk = advancedChallenge.slice(advancedChallenge.indexOf("Apple"),advancedChallenge.indexOf("Total:"));
+let advancedChallengeArray = advancedChallengeArrayChunk.split(",");
+let advancedChallengeTotalChunk = advancedChallenge.slice(advancedChallenge.indexOf("Total:"));
+let advancedChallengeTotal = advancedChallengeTotalChunk.toUpperCase();
+
+console.log(advancedChallengeName + ", " + advancedChallengeArray + ", " + advancedChallengeTotal);
+
+/*
 
 Practice Problem #2
 
